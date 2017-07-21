28005e6b1b6e794bc1d09cdf4d9a354247db6663
Nacho Capodanno <icapodanno@flowcode.com.ar>
2017-07-21 12:40:56 -0300

    first rogelio commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rogelioFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rogelioFrontApp
+ */
+angular.module('rogelioFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
